6850c2f604aeeadfd62248523a78fd35b53a84cf
songyi <song21677@naver.com>
2023-07-23 16:24:29 +0900

    refactor: 회원가입 시 사용자 입력값 검증 (Validator, rejectValue, WebDataBinder 연습)

--- login/src/main/java/com/example/practice/MemberController.java
+++ login/src/main/java/com/example/practice/MemberController.java
@@ -2,41 +2,33 @@ package com.example.practice;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.ObjectUtils;
-import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.regex.Pattern;
 
 @RequiredArgsConstructor
 @Controller
 public class MemberController {
     private final MemberRepository memberRepository;
+    private final MemberValidator memberValidator;
+
+    @InitBinder
+    public void init(WebDataBinder dataBinder) {
+        dataBinder.addValidators(memberValidator);
+    }
+
     @GetMapping("/add")
     public String addMemberForm(@ModelAttribute("member") Member member) {
         return "addMemberForm";
     }
 
     @PostMapping("/add")
-    public String save(@ModelAttribute Member member, BindingResult bindingResult) {
-
-        // TypeMismatch 테스트
-        if (member.getTest() < 5) {
-            bindingResult.addError(new FieldError("member", "test", "5 이상이어야 합니다."));
-        }
-
-        // 영문자, 숫자, 특수문자 포함 최소 8~20자
-        String pattern = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$";
-        if (!StringUtils.hasText(member.getPassword())
-                || !(member.getPassword().matches(pattern))) {
-            bindingResult.addError(new FieldError("member", "password", "비밀번호는 영문자, 숫자, 특수문자를 포함해 최소 8~20자리여야 합니다."));
-        }
-
-
+    public String save(@Validated @ModelAttribute Member member, BindingResult bindingResult) {
+        // memberValidator.validate(member, bindingResult);
         memberRepository.save(member);
         return "addMemberForm";
     }
