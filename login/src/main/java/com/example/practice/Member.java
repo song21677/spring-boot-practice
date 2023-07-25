475e41ec49749b981c9b217be67424f67edc5293
songyi <song21677@naver.com>
2023-07-25 22:45:03 +0900

    refactor: Bean Validation, 폼 전달 받는 전용 객체 사용 연습

--- login/src/main/java/com/example/practice/Member.java
+++ login/src/main/java/com/example/practice/Member.java
@@ -1,15 +1,29 @@
 package com.example.practice;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 public class Member {
     private long id;
+
+    // 필수 정보
+    // 5 ~ 20자의 영문 소문자, 숫자와 특수기호(_), (-)만 사용 가능
+    // @NotBlank
+    // @Pattern(regexp = "^(?=.*[a-z])(?=.*\\d)(?=.*[_-])[a-z\\d_-]{5,20}$")
     private String loginId;
-    private int test;
+
+    // 필수 정보
+    // 영문 대/소문자, 숫자, 특수문자를 포함한 8 ~ 16자
+    // @NotBlank
+    // @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,16}$")
     private String password;
+
+    // 필수 정보
+    // @NotBlank
     private String name;
 
     public Member() {}
-    public Member(long id, String loginId, String password, String name) {
-        this.id = id;
+    public Member(String loginId, String password, String name) {
         this.loginId = loginId;
         this.password = password;
         this.name = name;
@@ -45,13 +59,5 @@ public class Member {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public int getTest() {
-        return test;
-    }
-
-    public void setTest(int test) {
-        this.test = test;
     }
 }
--- login/src/main/java/com/example/practice/MemberController.java
+++ login/src/main/java/com/example/practice/MemberController.java
@@ -1,35 +1,35 @@
 package com.example.practice;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @RequiredArgsConstructor
 @Controller
 public class MemberController {
     private final MemberRepository memberRepository;
-    private final MemberValidator memberValidator;
-
-    @InitBinder
-    public void init(WebDataBinder dataBinder) {
-        dataBinder.addValidators(memberValidator);
-    }
 
     @GetMapping("/add")
     public String addMemberForm(@ModelAttribute("member") Member member) {
         return "addMemberForm";
     }
 
+//    @PostMapping("/add")
+//    public String save(@Validated @ModelAttribute Member member, BindingResult bindingResult) {
+//        memberRepository.save(member);
+//        return "addMemberForm";
+//    }
+
     @PostMapping("/add")
-    public String save(@Validated @ModelAttribute Member member, BindingResult bindingResult) {
-        // memberValidator.validate(member, bindingResult);
-        memberRepository.save(member);
+    public String save(@Validated @ModelAttribute("member") MemberSaveForm form, BindingResult bindingResult) {
+        Member member = new Member(form.getLoginId(), form.getPassword(), form.getName());
+        Member save = memberRepository.save(member);
         return "addMemberForm";
     }
 }
