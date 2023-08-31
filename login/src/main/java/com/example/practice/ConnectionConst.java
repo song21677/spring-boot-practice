f9fcc2c7eb31d3896cc7af10be52a1fa902e096a
songyi <song21677@naver.com>
2023-08-31 16:39:32 +0900

    chore: db 설정 정보 수정

--- login/src/main/java/com/example/practice/ConnectionConst.java
+++ login/src/main/java/com/example/practice/ConnectionConst.java
@@ -1,7 +1,7 @@
 package com.example.practice;
 
 public abstract class ConnectionConst {
-    public static final String URL = "jdbc:h2:tcp://localhost/~/test";
+    public static final String URL = "jdbc:h2:tcp://localhost/~/Desktop/gitRepo/spring-boot-practice/H2/test";
     public static final String USERID = "sa";
     public static final String PASSWORD = "";
 }
